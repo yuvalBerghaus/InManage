94742b0a5874f34178d46a740d0050a8ace06c02
Yuval Berghaus <65304080+yuvalBerghaus@users.noreply.github.com>
2023-10-01 20:04:46 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,13 +3,19 @@ require './controller/controller.php';
 
 
 /*
-Run tasks in chronological order
+Run tasks in Chronological order.
+After running each task comment it back.
 */
 
+// Step 1
 // CreateTablesAndInsertApiTask();
+// Step 2
 // SaveImageFromURLTask();
+// Step 3
 // DisplayPostsTask();
+// Step 4
 // DisplayLatestPostTask();
+// Step 5
 // CreateTablePostsPerHourTask();
 
 ?>
